644f55e0571544cb1b572e0e41807f6a7ae695ec
SJ <koloces@163.com>
2021-09-03 15:44:25 +0800

    添加下载模块

--- app/src/main/java/com/cqteam/network/NetWorkMainActivity.kt
+++ app/src/main/java/com/cqteam/network/NetWorkMainActivity.kt
@@ -2,10 +2,15 @@ package com.cqteam.network
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Toast
 import com.cqteam.networklib.NetWorkManager
 import com.cqteam.networklib.http.net.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.io.File
 
 class NetWorkMainActivity : AppCompatActivity() {
 
@@ -21,29 +26,76 @@ class NetWorkMainActivity : AppCompatActivity() {
             weather()
         }
     }
-    private fun start(){
-        requestAsync {
+
+    private fun start() {
+        val request = requestAsync {
             api.startApp2()
         }.onStart {
             Print.e("start----->")
         }.onSuccess {
             Print.e("success----->${it}")
-            Toast.makeText(this,"成功",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "成功", Toast.LENGTH_LONG).show()
         }.onFailure {
             Print.e("failure----->${it}")
-            Toast.makeText(this,"失败",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "失败", Toast.LENGTH_LONG).show()
             return@onFailure true
         }.onFinished {
             Print.e("onFinished----->")
         }
-    }
-    private fun weather(){
-        NetWorkRequest.request(api.getWeather(),false,{
-            Print.e("$it")
-        },{errCode, msg ->  },{},{})
+        GlobalScope.launch {
+            delay(10)
+            request.cancel()
+        }
+
     }
 
-    suspend fun getTest():String{
+    private fun weather() {
+//        GlobalScope.launch {
+//            val download = download(
+////                "https://img0.baidu.com/it/u=3948309700,2415409281&fm=26&fmt=auto&gp=0.jpg",
+//                "https://img0.baidu.com/u=3948309700,2415409281&fm=26&fmt=auto&gp=0.jpg",
+//                Environment.getExternalStorageDirectory().absolutePath + File.separator + "AAATestDownload",
+//                "test.jpg"
+//            )
+//            if (download == null){
+//                Print.e("下载失败")
+//            } else {
+//                Print.e("下载成功")
+//            }
+//        }
+
+        val download = downloadAsync(
+            "https://img0.baidu.com/it/u=3948309700,2415409281&fm=26&fmt=auto&gp=0.jpg",
+//            "https://img0.baidu.com/u=3948309700,2415409281&fm=26&fmt=auto&gp=0.jpg",
+            Environment.getExternalStorageDirectory().absolutePath + File.separator + "AAATestDownload",
+            "test.jpg"
+        ).onStart {
+            Print.e("onStart--------->")
+            Toast.makeText(this,"下载开始",Toast.LENGTH_SHORT).show()
+        }.onSuccess {
+            Print.e("onSuccess--------->")
+            Toast.makeText(this,"下载成功",Toast.LENGTH_SHORT).show()
+        }.onFailure {
+            Print.e("onFailure--------->$it")
+            Toast.makeText(this,"下载失败",Toast.LENGTH_SHORT).show()
+            return@onFailure false
+        }.onFinished {
+            Print.e("onFinished--------->")
+            Toast.makeText(this,"下载结束",Toast.LENGTH_SHORT).show()
+        }.onProgress { total, currentProgress ->
+            Print.e("onProgress--------->total:$total--currentProgress:$currentProgress")
+            runOnUiThread {
+                Toast.makeText(this,"下载进度:$currentProgress/$total",Toast.LENGTH_SHORT).show()
+            }
+        }
+        GlobalScope.launch {
+            delay(10)
+            Print.e("取消下载")
+            download.cancel()
+        }
+    }
+
+    suspend fun getTest(): String {
         return "结果"
     }
 }
--- networklib/src/main/java/com/cqteam/networklib/http/net/HttpResult.kt
+++ networklib/src/main/java/com/cqteam/networklib/http/net/HttpResult.kt
@@ -1,7 +1,13 @@
 package com.cqteam.networklib.http.net
 
 import android.util.Log
+import com.cqteam.networklib.http.okhttp.OkHttpClientProvider
 import kotlinx.coroutines.*
+import okhttp3.Request
+import okhttp3.Response
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
 import java.io.Serializable
 
 /**
@@ -17,6 +23,7 @@ class HttpResult<T> private constructor() : Serializable {
     internal var successBlock: ((value: T) -> Unit)? = null
     internal var failedBlock: ((value: Failure) -> Boolean)? = null
     internal var finishedBlock: (() -> Unit)? = null
+    internal var progressBlock: ((total : Long,currentProgress : Long) -> Unit)? = null
 
     internal val isLoading: Boolean get() = mValue == null
     internal val isFinished: Boolean get() = mValue != null
@@ -45,7 +52,7 @@ class HttpResult<T> private constructor() : Serializable {
         return mValue as Failure
     }
 
-    fun cancelJob() {
+    fun cancel() {
         netJob?.cancel()
     }
 
@@ -81,9 +88,11 @@ public suspend inline fun <T> request(
                 }
             }
         }.onFailure {
+            Log.e("HttpResult","请求失败-》${it}")
             return null
         }
     }.onFailure {
+        Log.e("HttpResult","请求失败-》${it}")
         return null
     }
     return null
@@ -111,9 +120,158 @@ public fun <T> requestAsync(block: suspend () -> BaseResponse<T>): HttpResult<T>
             }
             create.finishedBlock?.invoke()
         }.onFailure {
-            val onFailed = create.onFailed(-999, "Exception错误", it)
+            val onFailed = create.onFailed(-999, it.message?:"Exception错误", it)
             create.failedBlock?.invoke(onFailed)
             create.finishedBlock?.invoke()
+        }
+    }
+    create.setNetJob(job)
+    job.start()
+    return create
+}
+
+
+public suspend fun download(imgUrl:String,saveDir:String,saveName:String) : File? {
+    var inputStream: InputStream? = null
+    var fileOutputStream: FileOutputStream? = null
+    try {
+        val client = OkHttpClientProvider.getClient()
+        val request = Request.Builder().url(imgUrl).build()
+        val response: Response = client.newCall(request).execute()
+        if (!response.isSuccessful) {
+            Log.e("HttpResult","下载失败-->code:${response.code}")
+            return null
+        }
+        inputStream = response.body?.byteStream()
+        val buf: ByteArray = ByteArray(2048)
+        var len = 0
+        // 存储下载文件的目录
+        var file = File(saveDir)
+        if (!file.mkdirs()) {
+            file.createNewFile()
+        }
+        val savePath = saveDir + File.separator + saveName
+        file = File(savePath)
+        //判断文件名是否已存在，若已经存在则取时间戳重新建一个
+        if (file.exists()) {
+            file = File(saveDir + File.separator + System.currentTimeMillis() + "_" + saveName)
+        }
+        fileOutputStream = FileOutputStream(file)
+        if (inputStream != null) {
+            while (inputStream!!.read(buf).also { len = it } != -1) {
+                fileOutputStream!!.write(buf, 0, len)
+            }
+            fileOutputStream!!.flush()
+            // 下载完成
+            inputStream?.close()
+            fileOutputStream?.close()
+            return file
+        }
+    }catch (e:Exception){
+        try {
+            inputStream?.close()
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+
+        try {
+            fileOutputStream?.close()
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+        Log.e("HttpResult","下载失败-》${e}")
+        return null
+    }
+    return null
+}
+
+public fun downloadAsync(imgUrl:String,saveDir:String,saveName:String) : HttpResult<File>{
+    val create = HttpResult.create<File>()
+    val job = GlobalScope.launch(Dispatchers.IO,start = CoroutineStart.LAZY) {
+        var inputStream: InputStream? = null
+        var fileOutputStream: FileOutputStream? = null
+        try {
+            if (!isActive){
+                GlobalScope.launch(Dispatchers.Main) {
+                    val onFailed = create.onFailed(-999, "下载取消")
+                    create.failedBlock?.invoke(onFailed)
+                    create.finishedBlock?.invoke()
+                }
+                return@launch
+            }
+            val client = OkHttpClientProvider.getClient()
+            val request = Request.Builder().url(imgUrl).build()
+            val response: Response = client.newCall(request).execute()
+            if (!response.isSuccessful) {
+                val onFailed = create.onFailed(-998, "下载失败")
+                GlobalScope.launch(Dispatchers.Main) {
+                    create.failedBlock?.invoke(onFailed)
+                    create.finishedBlock?.invoke()
+                }
+                return@launch
+            }
+            inputStream = response.body?.byteStream()
+            val total = response.body?.contentLength()
+            val buf: ByteArray = ByteArray(2048)
+            var len = 0
+            // 存储下载文件的目录
+            var file = File(saveDir)
+            if (!file.mkdirs()) {
+                file.createNewFile()
+            }
+            val savePath = saveDir + File.separator + saveName
+            file = File(savePath)
+            //判断文件名是否已存在，若已经存在则取时间戳重新建一个
+            if (file.exists()) {
+                file = File(saveDir + File.separator + System.currentTimeMillis() + "_" + saveName)
+            }
+            fileOutputStream = FileOutputStream(file)
+            var sum: Long = 0
+            if (inputStream != null) {
+                while (inputStream.read(buf).also { len = it } != -1) {
+                    if (isActive) {
+                        fileOutputStream.write(buf, 0, len)
+                        sum += len
+                        total?.let { create.progressBlock?.invoke(it, sum) }
+                    } else {
+                        break
+                    }
+                }
+                fileOutputStream.flush()
+                // 下载完成
+                inputStream.close()
+                fileOutputStream.close()
+                create.setValue(file)
+                if (isActive) {
+                    GlobalScope.launch(Dispatchers.Main) {
+                        create.successBlock?.invoke(file)
+                        create.finishedBlock?.invoke()
+                    }
+                } else {
+                    GlobalScope.launch(Dispatchers.Main) {
+                        val onFailed = create.onFailed(-999, "下载取消")
+                        create.failedBlock?.invoke(onFailed)
+                        create.finishedBlock?.invoke()
+                    }
+                }
+            }
+        }catch (e:Exception){
+            try {
+                inputStream?.close()
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+
+            try {
+                fileOutputStream?.close()
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+            val onFailed = create.onFailed(-997, "下载失败",e)
+            GlobalScope.launch(Dispatchers.Main) {
+                create.failedBlock?.invoke(onFailed)
+                create.finishedBlock?.invoke()
+            }
         }
     }
     create.setNetJob(job)
@@ -150,10 +308,19 @@ public fun <T> HttpResult<T>.onSuccess(action: (value: T) -> Unit): HttpResult<T
     return this
 }
 
+/**
+ * 这个回调是在子线程
+ */
+public fun <T> HttpResult<T>.onProgress(action: (total : Long,currentProgress : Long) -> Unit) : HttpResult<T>{
+    progressBlock = action
+    return this
+}
+
 public inline fun <T> HttpResult<T>.onStart(action: () -> Unit): HttpResult<T> {
     action.invoke()
     return this
 }
+
 
 public fun <T> HttpResult<T>.onFinished(action: () -> Unit): HttpResult<T> {
     if (isFinished) {
